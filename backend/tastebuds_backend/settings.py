d559628bba847a5afeb16d35b4c89e82015582fa
Chen <chenylle.geronimo@gmail.com>
2026-05-01 17:49:44 +0100

    Use Resend instead

--- backend/tastebuds_backend/settings.py
+++ backend/tastebuds_backend/settings.py
@@ -155,11 +155,3 @@ CSRF_TRUSTED_ORIGINS = [
     'https://tastebuds-production-674f.up.railway.app',
     'https://tastebuds-gray.vercel.app',
 ]
-
-# CONTACT US
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'chenylle.geronimo@gmail.com'
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
